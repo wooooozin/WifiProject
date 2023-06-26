7f02282116caf46a98664acc8c0cc2618c6d8a4b
wooooozin <wooooozin@gmail.com>
2023-06-27 07:50:51 +0900

    httpClient 설치

--- WifiDemo/WiFinder/src/main/java/api/Api.java
+++ WifiDemo/WiFinder/src/main/java/api/Api.java
@@ -1,10 +1,30 @@
 package api;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.jws.soap.SOAPBinding;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.net.URIBuilder;
+
 public class Api {
+    final static String KEY = "74664261736f6f393731714449467a";
+    final static String BASE_URL = "http://openapi.seoul.go.kr:8088/" + KEY + "/json/TbPublicWifiInfo/";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void getWifiInfo(int s, int e) {
+        
+    }
 
-	}
+    public static void main(String[] args) {
 
+        getWifiInfo(1, 10);
+    }
 }
