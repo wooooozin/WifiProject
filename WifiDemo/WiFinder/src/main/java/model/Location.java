4e6d788937cb01892ac24c0f221bd6ad07db8e65
wooooozin <wooooozin@gmail.com>
2023-07-01 22:21:47 +0900

    location class 모델 구현

--- WifiDemo/WiFinder/src/main/java/model/Location.java
+++ WifiDemo/WiFinder/src/main/java/model/Location.java
@@ -1,6 +1,43 @@
 package model;
 
 public class Location {
+	private int locationId;
+	private String latitude;
+	private String longitude;
+	
+	public int getLocationId() {
+		return locationId;
+	}
+	
+	public void setLocationId(int locationId) {
+		this.locationId = locationId;
+	}
+	
+	public String getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	
+	public String getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	public String getConfirmDate() {
+		return confirmDate;
+	}
+	
+	public void setConfirmDate(String confirmDate) {
+		this.confirmDate = confirmDate;
+	}
+	
+	private String confirmDate;
 	
 
 }
