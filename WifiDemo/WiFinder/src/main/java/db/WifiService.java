d3443bd421bf2c37fe2d13d6e6ce578c5364d759
wooooozin <wooooozin@gmail.com>
2023-07-02 23:35:49 +0900

    변수명 변경

--- WifiDemo/WiFinder/src/main/java/db/WifiService.java
+++ WifiDemo/WiFinder/src/main/java/db/WifiService.java
@@ -1,6 +1,8 @@
 package db;
 
+import java.lang.management.OperatingSystemMXBean;
 import java.sql.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -197,26 +199,26 @@ public class WifiService {
                 String lnt = rs.getString("lnt");
                 String workDttm = rs.getString("work_dttm");
                 
-                Wifi location = new Wifi();
-                location.setDistance(distance);
-                location.setManagerNumber(mgrNo);
-                location.setWardOffice(wrdofc);
-                location.setMainName(mainNm);
-                location.setAddress1(address1);
-                location.setAddress2(address2);
-                location.setInstallationFloor(instlFloor);
-                location.setInstallationType(instlTy);
-                location.setInstallationBy(instlMby);
-                location.setServiceType(svcSe);
-                location.setNetworkType(cmcwr);
-                location.setConstructionYear(cnstcYear);
-                location.setIndoorOutdoor(inoutDoor);
-                location.setWifiEnvironment(remars3);
-                location.setLatitude(lat);
-                location.setLongitude(lnt);
-                location.setWorkDateTime(workDttm);
+                Wifi wifi = new Wifi();
+                wifi.setDistance(distance);
+                wifi.setManagerNumber(mgrNo);
+                wifi.setWardOffice(wrdofc);
+                wifi.setMainName(mainNm);
+                wifi.setAddress1(address1);
+                wifi.setAddress2(address2);
+                wifi.setInstallationFloor(instlFloor);
+                wifi.setInstallationType(instlTy);
+                wifi.setInstallationBy(instlMby);
+                wifi.setServiceType(svcSe);
+                wifi.setNetworkType(cmcwr);
+                wifi.setConstructionYear(cnstcYear);
+                wifi.setIndoorOutdoor(inoutDoor);
+                wifi.setWifiEnvironment(remars3);
+                wifi.setLatitude(lat);
+                wifi.setLongitude(lnt);
+                wifi.setWorkDateTime(workDttm);
                               
-                wifis.add(location);
+                wifis.add(wifi);
             }
 
 	        
@@ -248,6 +250,104 @@ public class WifiService {
 	        }
 	    }
 	    return wifis;
+	}
+	
+	public static Wifi showDetailWifiInfo(String manageNumber) {
+		Wifi wifiInfo = new Wifi();
+		
+		try {
+	        Class.forName("org.mariadb.jdbc.Driver");
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
+
+	    Connection connection = null;
+	    PreparedStatement preparedStatement = null;
+	    ResultSet rs = null;
+
+	    try {
+	        connection = DriverManager.getConnection(url, dbUserId, dbPassword);
+	        if (connection == null) {
+	            throw new SQLException("Failed to establish a database connection.");
+	        }
+
+	        String sql = " SELECT * FROM wifi_info "
+	        		+ "WHERE mgr_no = ? ; ";
+
+	        preparedStatement = connection.prepareStatement(sql);
+	        preparedStatement.setString(1, manageNumber);
+	        rs = preparedStatement.executeQuery(); 
+           
+	        while (rs.next()) {
+                String distance = rs.getString("distance");
+                String mgrNo = rs.getString("mgr_no");
+                String wrdofc = rs.getString("wrdofc");
+                String mainNm = rs.getString("main_nm");
+                String address1 = rs.getString("adress1");
+                String address2 = rs.getString("adress2");
+                String instlFloor = rs.getString("instl_floor");
+                String instlTy = rs.getString("instl_ty");
+                String instlMby = rs.getString("instl_mby");
+                String svcSe = rs.getString("svc_se");
+                String cmcwr = rs.getString("cmcwr");
+                String cnstcYear = rs.getString("cnstc_year");
+                String inoutDoor = rs.getString("inout_door");
+                String remars3 = rs.getString("remars3");
+                String lat = rs.getString("lat");
+                String lnt = rs.getString("lnt");
+                String workDttm = rs.getString("work_dttm");
+                
+                wifiInfo.setDistance(distance);
+                wifiInfo.setManagerNumber(mgrNo);
+                wifiInfo.setWardOffice(wrdofc);
+                wifiInfo.setMainName(mainNm);
+                wifiInfo.setAddress1(address1);
+                wifiInfo.setAddress2(address2);
+                wifiInfo.setInstallationFloor(instlFloor);
+                wifiInfo.setInstallationType(instlTy);
+                wifiInfo.setInstallationBy(instlMby);
+                wifiInfo.setServiceType(svcSe);
+                wifiInfo.setNetworkType(cmcwr);
+                wifiInfo.setConstructionYear(cnstcYear);
+                wifiInfo.setIndoorOutdoor(inoutDoor);
+                wifiInfo.setWifiEnvironment(remars3);
+                wifiInfo.setLatitude(lat);
+                wifiInfo.setLongitude(lnt);
+                wifiInfo.setWorkDateTime(workDttm);
+                System.out.println(distance);
+
+            }
+
+	        
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (rs != null && !rs.isClosed()) {
+	                rs.close();
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+
+	        try {
+	            if (preparedStatement != null && !preparedStatement.isClosed()) {
+	                preparedStatement.close();
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+
+	        try {
+	            if (connection != null && !connection.isClosed()) {
+	                connection.close();
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+		
+		return wifiInfo;
 	}
 	
 	public static boolean hasData() {
