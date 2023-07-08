b002ec81393eda4740cd8aab7b1a7302e283bb0f
wooooozin <wooooozin@gmail.com>
2023-07-08 20:42:53 +0900

    북마크 등록 리스트 구현

--- WifiDemo/WiFinder/src/main/java/db/BookmarkListSerivce.java
+++ WifiDemo/WiFinder/src/main/java/db/BookmarkListSerivce.java
@@ -11,76 +11,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.Bookmark;
-import model.Wifi;
+import model.BookmarkList;
 
-public class BookmarkService {
-
+public class BookmarkListSerivce {
+	
 	private static String url = "jdbc:mariadb://localhost:3306/wifi";
 	private static String dbUserId = "wifiuser";
 	private static String dbPassword = "wifi";
 	
-	public static void insertBookmarkInfo(String bookmarkName, String priority) {
-		if (bookmarkName.isEmpty() || priority.isEmpty()) {
-	        return;
-	    }
-		
-	    try {
-	        Class.forName("org.mariadb.jdbc.Driver");
-	    } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
-	    }
-	    
-	    Connection connection = null;
-	    PreparedStatement preparedStatement = null;
-	    try { 
-	        connection = DriverManager.getConnection(url, dbUserId, dbPassword);
-	        if (connection == null) {
-	            throw new SQLException("Failed to establish a database connection.");
-	        }
-	        
-	        LocalDateTime now = LocalDateTime.now();
-	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	        String currentTime = now.format(formatter);
-	        
-	        String sql = " INSERT INTO bookmark_info "
-	        		+ " (bookmark_name, priority, regit_date, modif_date) "
-	        		+ " VALUES (?, ?, ?, ?, ?) ; ";
-            preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, bookmarkName);
-            preparedStatement.setInt(2, Integer.parseInt(priority));
-            preparedStatement.setString(3, currentTime);
-            preparedStatement.setString(4, "");
-            
-            int affectedRows = preparedStatement.executeUpdate();
-            if (affectedRows > 0) {
-                System.out.println("저장 성공");
-            } else {
-                System.out.println("저장 실패");
-            }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            if (preparedStatement != null && !preparedStatement.isClosed()) {
-	                preparedStatement.close();
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException(e);
-	        }
-	        
-	        try {
-	            if (connection != null && !connection.isClosed()) {
-	                connection.close();
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
-	    
-	}
 	
-	public static List<Bookmark> showBookmarkInfoOderByPriority() {
-		List<Bookmark> bookmarks = new ArrayList<>();
+	// 정보 조회하기 
+	public static List<BookmarkList> showBookmarkLists() {
+		List<BookmarkList> bookmarkLists = new ArrayList<>();
 		
 		try {
 	        Class.forName("org.mariadb.jdbc.Driver");
@@ -98,27 +40,27 @@ public class BookmarkService {
 	            throw new SQLException("Failed to establish a database connection.");
 	        }
 
-	        String sql = " SELECT * FROM bookmark_info "
-	        		+ " ORDER BY priority; ";
+	        String sql = " SELECT bl.bookmark_list_id, b.bookmark_name, w.main_nm, bl.regit_date FROM bookmark_list bl, wifi_info w, bookmark_info b "
+	        		+ "WHERE bl.wifi_id = w.wifi_id "
+	        		+ "AND bl.bookmark_id = b.bookmark_id ; ";
 
 	        preparedStatement = connection.prepareStatement(sql);
 	        rs = preparedStatement.executeQuery(); 
            
 	        while (rs.next()) {
-                String id = rs.getString("bookmark_id");
-                String name = rs.getString("bookmark_name");
-                int priority = rs.getInt("priority");
+                String id = rs.getString("bookmark_list_id");
+                String bookmarkName = rs.getString("bookmark_name");
+                String wifiName = rs.getString("main_nm");
                 String regitDate = rs.getString("regit_date");
-                String modifDate = rs.getString("modif_date");
                 
                 
-                Bookmark bookmark = new Bookmark();
-                bookmark.setBookmarkId(id);
-                bookmark.setBookmarkName(name);
-                bookmark.setPriority(String.valueOf(priority));
-                bookmark.setRegitDate(regitDate);
-                bookmark.setModifDate(modifDate);
-                bookmarks.add(bookmark);
+                BookmarkList bookmarkList = new BookmarkList();
+                bookmarkList.setBookmarkListId(id);
+                bookmarkList.setBookmarkName(bookmarkName);
+                bookmarkList.setWifiMainName(wifiName);
+                bookmarkList.setRegitDateS(regitDate);
+                
+                bookmarkLists.add(bookmarkList);
             }
 
 	        
@@ -150,10 +92,70 @@ public class BookmarkService {
 	        }
 	    }
 	    
-		return bookmarks;
+		return bookmarkLists;
 		
 	}
 	
+	// 데이터 베이스 추가
+	public static void insertBookmarkInfo(String wifiId, String bookmarkId) {
+		if (wifiId.isEmpty() || bookmarkId.isEmpty()) {
+	        return;
+	    }
+		
+	    try {
+	        Class.forName("org.mariadb.jdbc.Driver");
+	    } catch (ClassNotFoundException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    Connection connection = null;
+	    PreparedStatement preparedStatement = null;
+	    try { 
+	        connection = DriverManager.getConnection(url, dbUserId, dbPassword);
+	        if (connection == null) {
+	            throw new SQLException("Failed to establish a database connection.");
+	        }
+	        
+	        LocalDateTime now = LocalDateTime.now();
+	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	        String currentTime = now.format(formatter);
+	        
+	        String sql = " INSERT INTO bookmark_list (wifi_id, bookmark_id, regit_date) "
+	        		+ " VALUES( ?, ?,? ) ; ";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, Integer.parseInt(wifiId));
+            preparedStatement.setInt(2, Integer.parseInt(bookmarkId));
+            preparedStatement.setString(3, currentTime);
+            
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("저장 성공");
+            } else {
+                System.out.println("저장 실패");
+            }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            if (preparedStatement != null && !preparedStatement.isClosed()) {
+	                preparedStatement.close();
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+	        
+	        try {
+	            if (connection != null && !connection.isClosed()) {
+	                connection.close();
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	    
+	}
+	
+	// 삭제 하기 
 	public static void deleteLocationInfo(String id) {
 		try {
 	        Class.forName("org.mariadb.jdbc.Driver");
@@ -169,8 +171,8 @@ public class BookmarkService {
             // 커넥션 객체 생성
             connection = DriverManager.getConnection(url, dbUserId, dbPassword);
             // 쿼리 생성
-            String sql = " DELETE FROM bookmark_info "
-            		+ "WHERE bookmark_id = ? ; ";
+            String sql = " DELETE FROM bookmark_list "
+            		+ "WHERE bookmark_list_id = ? ; ";
 
             int idValue = Integer.parseInt(id);
             
@@ -210,73 +212,8 @@ public class BookmarkService {
         }
 	}
 	
-	public static void updateBookmarkInfo(String id, String name, String priority) {
-		try {
-	        Class.forName("org.mariadb.jdbc.Driver");
-	    } catch (ClassNotFoundException e) {
-	        e.printStackTrace();
-	    }
-
-	    Connection connection = null;
-	    PreparedStatement preparedStatement = null;
-	    ResultSet rs = null;
-	    
-	    try {
-	        connection = DriverManager.getConnection(url, dbUserId, dbPassword);
-	        if (connection == null) {
-	            throw new SQLException("Failed to establish a database connection.");
-	        }
-	        
-	        String sql = "UPDATE bookmark_info "
-	        		+ " SET bookmark_name = ?, priority = ?, modif_date = ? "
-	        		+ " WHERE bookmark_id = ? ; ";
-	        LocalDateTime now = LocalDateTime.now();
-	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-	        String currentTime = now.format(formatter);
-	        
-	        preparedStatement = connection.prepareStatement(sql);
-	        preparedStatement.setString(1, name);
-	        preparedStatement.setInt(2, Integer.parseInt(priority));
-	        preparedStatement.setString(3, currentTime);
-	        preparedStatement.setInt(4, Integer.parseInt(id));
-	        
-	        int rowsAffected = preparedStatement.executeUpdate();
-	        if (rowsAffected > 0) {
-	            System.out.println("수정 성공");
-	        } else {
-	            System.out.println("수정 실패");
-	        }
-            connection.commit(); 	        
-
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    } finally {
-	        try {
-	            if (rs != null && !rs.isClosed()) {
-	                rs.close();
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException(e);
-	        }
-
-	        try {
-	            if (preparedStatement != null && !preparedStatement.isClosed()) {
-	                preparedStatement.close();
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException(e);
-	        }
-
-	        try {
-	            if (connection != null && !connection.isClosed()) {
-	                connection.close();
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException(e);
-	        }
-	    }
-	}
 	
+	// 데이터 있나 없나 확인
 	public static boolean hasData() {
 	    try {
 	        Class.forName("org.mariadb.jdbc.Driver");
@@ -295,7 +232,7 @@ public class BookmarkService {
 	            throw new SQLException("Failed to establish a database connection.");
 	        }
 
-	        String sql = "SELECT COUNT(*) FROM bookmark_info";
+	        String sql = "SELECT COUNT(*) FROM bookmark_list";
 
 	        preparedStatement = connection.prepareStatement(sql);
 	        rs = preparedStatement.executeQuery();
---
+++ WifiDemo/WiFinder/src/main/java/model/BookmarkList.java
@@ -0,0 +1,42 @@
+package model;
+
+public class BookmarkList {
+	private String bookmarkListId;
+	private String bookmarkName;
+	private String wifiMainName;
+	private String regitDateS;
+	
+	public String getBookmarkListId() {
+		return bookmarkListId;
+	}
+	
+	public void setBookmarkListId(String bookmarkListId) {
+		this.bookmarkListId = bookmarkListId;
+	}
+	
+	public String getBookmarkName() {
+		return bookmarkName;
+	}
+	
+	public void setBookmarkName(String bookmarkName) {
+		this.bookmarkName = bookmarkName;
+	}
+	
+	public String getWifiMainName() {
+		return wifiMainName;
+	}
+	
+	public void setWifiMainName(String wifiMainName) {
+		this.wifiMainName = wifiMainName;
+	}
+	
+	public String getRegitDateS() {
+		return regitDateS;
+	}
+	
+	public void setRegitDateS(String regitDateS) {
+		this.regitDateS = regitDateS;
+	}
+	
+	
+}
